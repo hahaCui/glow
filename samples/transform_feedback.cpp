3d6f544eb90acf6ddb487b9dc6a5bab98dc5ae90
pangfumin <pangfumin@gmail.com>
2019-05-26 22:27:07 +0800

    transform_feedback works fine

--- samples/transform_feedback.cpp
+++ samples/transform_feedback.cpp
@@ -35,68 +35,68 @@ int main(int argc, char** argv) {
 
     GlFramebuffer fbo(width, height);
 
-    glow::GlBuffer<vec4> surfels{glow::BufferTarget::ARRAY_BUFFER,
-                                    glow::BufferUsage::DYNAMIC_DRAW};  // feedback stores updated surfels inside surfels_.
+    glow::GlBuffer<vec4> input_vec{glow::BufferTarget::ARRAY_BUFFER,
+                                    glow::BufferUsage::DYNAMIC_DRAW};  // feedback stores updated input_vec inside input_vec.
 
-    glow::GlBuffer<vec4> extractBuffer_{glow::BufferTarget::ARRAY_BUFFER, glow::BufferUsage::DYNAMIC_DRAW};
-    glow::GlProgram extractProgram_;
-    glow::GlTransformFeedback extractFeedback_;
+    glow::GlBuffer<vec4> extractBuffer{glow::BufferTarget::ARRAY_BUFFER, glow::BufferUsage::DYNAMIC_DRAW};
+    glow::GlProgram extractProgram;
+    glow::GlTransformFeedback extractFeedback;
 
     std::vector<vec4> vec;
     vec.push_back(vec4(1,0,1,0));
     vec.push_back(vec4(1,1,1,1));
     vec.push_back(vec4(1,-1,0, 1));
-    surfels.assign(vec);
-    std::cout << "surfels: " << surfels.size() << std::endl;
+    input_vec.assign(vec);
+    std::cout << "input_vec: " << input_vec.size() << std::endl;
 
     std::vector<std::string> varyings{
             "position_out",
     };
-    surfels.reserve(1000);
-    extractFeedback_.attach(varyings, surfels);
+    extractBuffer.reserve(100);
+    extractFeedback.attach(varyings, extractBuffer);
 
-    glow::GlVertexArray vao_surfels_;
-    // now we can set the vertex attributes. (the "shallow copy" of surfels now contains the correct id.
-    vao_surfels_.setVertexAttribute(0, surfels, 4, AttributeType::FLOAT, false, sizeof(vec4),
+    glow::GlVertexArray vao_input_vec;
+    // now we can set the vertex attributes. (the "shallow copy" of input_vec now contains the correct id.
+    vao_input_vec.setVertexAttribute(0, input_vec, 4, AttributeType::FLOAT, false, sizeof(vec4),
                                     reinterpret_cast<GLvoid*>(0));
 
 
-    extractProgram_.attach(GlShader::fromFile(ShaderType::VERTEX_SHADER, "/home/pang/suma_ws/src/glow/samples/shader/extract_surfels.vert"));
-    extractProgram_.attach(GlShader::fromFile(ShaderType::FRAGMENT_SHADER, "/home/pang/suma_ws/src/glow/samples/shader/empty.frag"));
-    extractProgram_.attach(extractFeedback_);
-    extractProgram_.link();
+    extractProgram.attach(GlShader::fromFile(ShaderType::VERTEX_SHADER, "/home/pang/suma_ws/src/glow/samples/shader/extract_vec.vert"));
+    extractProgram.attach(GlShader::fromFile(ShaderType::FRAGMENT_SHADER, "/home/pang/suma_ws/src/glow/samples/shader/empty.frag"));
+    extractProgram.attach(extractFeedback);
+    extractProgram.link();
 
 
-    extractFeedback_.bind();
-    extractProgram_.bind();
+    extractFeedback.bind();
+    extractProgram.bind();
 
     glEnable(GL_RASTERIZER_DISCARD);
 
-    extractProgram_.bind();
-    extractFeedback_.bind();
-    vao_surfels_.bind();
+    extractProgram.bind();
+    extractFeedback.bind();
+    vao_input_vec.bind();
 
 
 
-    extractFeedback_.begin(TransformFeedbackMode::POINTS);
-    glDrawArrays(GL_POINTS, 0, surfels.size());
-    uint32_t extractedSize = extractFeedback_.end();
+    extractFeedback.begin(TransformFeedbackMode::POINTS);
+    glDrawArrays(GL_POINTS, 0, input_vec.size());
+    uint32_t extractedSize = extractFeedback.end();
 
-    extractBuffer_.resize(extractedSize);
+    extractBuffer.resize(extractedSize);
 
-    vao_surfels_.release();
-    extractFeedback_.release();
-    extractProgram_.release();
+    std::vector<vec4> download_input_vec;
+    download_input_vec.reserve(3);
+    extractBuffer.get(download_input_vec);
+    std::cout << "download_input_vec: " << download_input_vec.size() << std::endl;
 
-
-    std::vector<vec4> download_surfels;
-    extractBuffer_.get(download_surfels);
-    std::cout << "download_surfels: " << download_surfels.size() << std::endl;
-
-    for (auto i : download_surfels) {
+    for (auto i : download_input_vec) {
         std::cout << i.x << " " << i.y << " " <<  i.z << " " << i.w << std::endl;
     }
-
+    
+    vao_input_vec.release();
+    extractFeedback.release();
+    extractProgram.release();
+    
     glDisable(GL_RASTERIZER_DISCARD);
 
     return 0;
